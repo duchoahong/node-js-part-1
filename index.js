bb220cc8608c5b1f65f148079a76eae2ebc5a792
duchoahong <49894182+duchoahong@users.noreply.github.com>
2019-05-20 14:21:14 +0700

    Start project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 9999;
+
+app.get('/', (req, res) => res.send('Hello hahahaha!'))
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
